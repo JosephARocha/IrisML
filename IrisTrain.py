aec1ddc45886fc41df4816da0ba2639c3eb52a65
Joseph Rocha <kkw293@my.utsa.edu>
2018-03-31 01:18:55 -0500

    Initial Commit

---
+++ IrisTrain.py
@@ -0,0 +1,41 @@
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.optimizers import SGD
+from keras.utils import to_categorical
+import numpy as np
+import pandas as pd
+
+# Read In Iris.data into an array
+dataset = pd.read_csv('Iris.data')
+
+#The y values are text, we turn them into integers to feed into the network
+dataset = dataset.replace("Iris-setosa", "0")
+dataset = dataset.replace("Iris-versicolor", "1")
+dataset = dataset.replace("Iris-virginica", "2")
+
+# columns 0-4 are our features (x values), column 5 is our y value... f(x1,x2,x3,x4) = y
+x_train = dataset.iloc[:,0:4]
+y_train = dataset.iloc[:,4:5]
+#Turn the y values into a matrix [0,0,1] [0,1,0] [1,0,0]
+y_train = to_categorical(y_train)
+
+#same for our text, this is lazy!!!! don't use the same values for testing! we want values that our model hasn't seen
+x_test = dataset.iloc[:,0:4]
+y_test = dataset.iloc[:,4:5]
+y_test = to_categorical(y_test)
+
+#simple sequential model
+model = Sequential()
+model.add(Dense(128, activation='relu', input_dim=4))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(3, activation='softmax'))
+
+#https://en.wikipedia.org/wiki/Stochastic_gradient_descent
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+#Train our model for 100 iteration of the training data
+model.fit(x_train, y_train, epochs=100)
+#evaluate model on our testing data, remember this is bad!!!
+score = model.evaluate(x_test, y_test)
